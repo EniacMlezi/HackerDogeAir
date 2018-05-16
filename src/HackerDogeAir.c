e2b4e3e09cceb944827c4c374437e98ae293b870
Lars Gardien <larsgardien@live.nl>
2018-05-16 21:29:05 +0200

    POST Form handling

--- src/HackerDogeAir.c
+++ src/HackerDogeAir.c
@@ -1,11 +1,31 @@
 #include <kore/kore.h>
 #include <kore/http.h>
 
-int		page(struct http_request *);
+int     loginpost(struct http_request *);
 
-int
-page(struct http_request *req)
+int 
+loginpost(struct http_request *req)
 {
-	http_response(req, 200, NULL, 0);
-	return (KORE_RESULT_OK);
+    char *username;
+    char *password;
+    struct kore_buf *buf;
+
+    buf = kore_buf_alloc(128); //TODO: check buff overflow @ append
+
+    http_populate_post(req);
+    if(http_argument_get_string(req, "username", &username))
+    {
+        kore_buf_appendf(buf, "username: '%s' \n", username);
+    }
+    if(http_argument_get_string(req, "password", &password))
+    {
+        kore_buf_appendf(buf, "password: '%s' \n", password);
+    }
+    kore_log(LOG_NOTICE, "username: '%s' password: '%s' ", username, password);
+
+    http_response_header(req, "content-type", "text/plain");
+    http_response(req, 200, buf->data, buf->offset);
+    kore_buf_free(buf);
+
+    return (KORE_RESULT_OK);
 }
