e06b55711048cf098a6717680ca0bc4e79dbfc8a
Dennis Smith <dennis17-17@hotmail.com>
2018-06-17 15:50:26 +0200

    Fixed several warnings and changed error handling

--- src/pages/home/home.c
+++ src/pages/home/home.c
@@ -13,7 +13,7 @@
 #include "assets.h"
 
 int    home(struct http_request *);
-void   home_error_handler(struct http_request *, int, SharedContext *);
+void   home_error_handler(struct http_request *, int);
 
 int 
 home(struct http_request *req)
@@ -22,42 +22,27 @@ home(struct http_request *req)
     SharedContext context = {
         .session_id = 0  //TODO: fill from request cookie
     };
-    if(req->method == HTTP_METHOD_GET)
-    {   //a GET receives the home form
-        if((err = home_render(&context)) != (SHARED_ERROR_OK))
-        {
-            home_error_handler(req, err, &context);
-        }
 
-        http_response_header(req, "content-type", "text/html");
-        http_response(req, HTTP_STATUS_OK, 
-            context.dst_context->string,
-            strlen(context.dst_context->string));
-
-        home_render_clean(&context);
-        return (KORE_RESULT_OK);
-    }
-    else if(req->method != HTTP_METHOD_POST)
-    {   //only serve GET and POST requests
-        return (KORE_RESULT_ERROR);
-    }
-
-    return (KORE_RESULT_OK);
-}
-
-void
-home_error_handler(struct http_request *req, int errcode, SharedContext *context)
-{
-    int err = 0;
-    if((err = home_render(context)) != (SHARED_ERROR_OK))
+    if(req->method != HTTP_METHOD_GET)  
+        return(KORE_RESULT_ERROR); //all methodes besides GET don't exist on the home page
+    
+    //a GET receives the home form and renders the page
+    if((err = home_render(&context)) != (SHARED_ERROR_OK))
     {
-        home_error_handler(req, err, context);
+        home_error_handler(req, err);
     }
 
     http_response_header(req, "content-type", "text/html");
     http_response(req, HTTP_STATUS_OK, 
-        *context->dst_context->string, 
-        strlen(*context->dst_context->string));
+        context.dst_context->string,
+        strlen(context.dst_context->string));
 
-    home_render_clean(context);
+    home_render_clean(&context);
+    return (KORE_RESULT_OK);    
+}
+
+void
+home_error_handler(struct http_request *req, int errcode)
+{
+    shared_error_handler(req, errcode);
 }
